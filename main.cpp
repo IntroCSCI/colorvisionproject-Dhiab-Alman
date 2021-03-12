5900aff32b21237daf79a766f59eb1e4907f06c1
Dhiab Almansoori <dmalmansoori1@mail.csuchico.edu>
2021-03-12 16:01:33 +0000

    g

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
-//Author: PUT YOUR NAME HERE
+//Author: Dhiab almansoori
+#include <fstream>
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+  string line = "";
+  size_t position;
+  string filename;
+  ifstream reader;
+  string color;
 
-  return 0;
-}
+   cout << "Please type the name of the file to open:\n";
+   getline(cin, filename);
+   reader.open(filename, ios::in);
+
+   if (reader.is_open())  {
+
+      while (!reader.eof()) {
+        getline(reader, line);
+        position = line.find("#");
+
+        if (line[position]) {
+          cout << '#';
+          for (char colorvalue = position += 1; (line[colorvalue] >= '0' && line[colorvalue]
+          <='9') || (line[colorvalue] >= 'A' && line[colorvalue]  <= 'F'); colorvalue++) {
+
+            cout << line[colorvalue];
+          }
+        }
+        cout << endl;
+      }
+   }
+
+
+
+
+
+  return 0;}
